ba1268ce36aff70deb7c94f708ac52a451d70b31
Nick Kamper <nick.kamper@blackboard.com>
2019-08-02 17:35:25 -0400

    Build out the initial LTI launch endpoint

---
+++ config_example.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // The base URL for this integration server, without trailing slash (example: http://localhost:3000)
+    integrationUrl: '',
+
+    // The application key retrieved from developer.blackboard.com.
+    // NOTE: This is not the application ID used to configure the REST API Integration in Learn
+    applicationKey: '',
+
+    // The application secret retrieved from developer.blackboard.com
+    applicationSecret: '',
+};
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const config = require(`./config`);
+
+// (1) This endpoint receives the LTI launch from the LMS. From here, we will redirect to begin the OAuth2 workflow (see #2)
+app.post('/lti-launch', (req, res) => {
+    // Normally, you'd want to validate the LTI request you receive here. However, we will omit this as an exercise to
+    // the reader (see Step 1-3 of https://www.imsglobal.org/wiki/step-1-lti-launch-request)
+
+    // We will use the launch_presentation_return_url to determine the hostname of the LMS. We are making some assumption
+    // that the LMS will be hosted at the root of the return URL, so a production-quality application will likely want to
+    // do something more robust here.
+    const url = new URL(req.body.launch_presentation_return_url);
+
+    // Now, we will build the return URL that will point the user back to the integration after the OAuth2 workflow (the
+    // URL for #2).
+    const redirectUri = `${config.integrationUrl}/authorization-complete`;
+
+    // Now, we will build the authorization code request URL. In a production scenario, you would want to add state,
+    // code_challenge, and code_challenge_method parameters for additional security.
+    const authorizationCodeParams = {
+        redirect_uri: redirectUri,
+        response_type: 'code',
+        client_id: config.applicationKey,
+    };
+    const authorizationCodeUrl = buildUrl(
+        `${url.protocol}://${url.host}/learn/api/public/v1/oauth2/authorizationcode`,
+        authorizationCodeParams
+    );
+
+    res.redirect(authorizationCodeUrl);
+});
+
+app.get('/authorization-complete', (req, res) => {
+
+});
+
+
+/**
+ * Builds a URL with query parameters
+ * @param baseUrl
+ * @param queryParams
+ * @returns {string}
+ */
+function buildUrl(baseUrl, queryParams) {
+    const paramsString = Object.keys(queryParams)
+        .map(i => `${i}=${queryParams[i]}`)
+        .join('&');
+
+    return `${baseUrl}?${paramsString}`;
+}
