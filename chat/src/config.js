5bf152e49d3cb62c954bc9b444c7f8c44a6bde27
Miles Exner <mexner@pacificcollege.edu>
2024-01-22 13:31:47 -0700

    🚀 add integration URL

--- chat/src/config.js
+++ chat/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // The base URL for this integration server, without trailing slash (example: http://localhost:8091)
-    integrationUrl: '',
+    integrationUrl: process.env.APP_URL || '',
 
     // The application key retrieved from developer.blackboard.com.
     // NOTE: This is not the application ID used to configure the REST API Integration in Learn
